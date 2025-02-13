a539368486eb931c0d9ba56865ee6084b82739d3
SmallQ <gongyunbo0119@outlook.com>
2025-02-13 20:09:47 +0800

    update commercial law

--- src/.vuepress/sidebar/en.ts
+++ src/.vuepress/sidebar/en.ts
@@ -4,38 +4,61 @@ export const enSidebar = sidebar({
   "/": [
     "",
     {
-      text: "Guide",
-      icon: "laptop-code",
-      prefix: "demo/",
-      link: "demo/",
-      children: "structure",
-    },
-    {
-      text: "Articles",
-      icon: "book",
-      prefix: "posts/",
-      children: "structure",
-    },
-    {
       text: "Legal Study",
       icon: "scale-balanced",
       prefix: "legal study/",
       link: "legal study/",
       children: [
         {
-          text: "Jurisprudence",  // English display name
-          prefix: "Jurisprudence/", // Keep the same folder name
+          text: "Jurisprudence",
+          icon: "gavel",
+          prefix: "Jurisprudence/",
           link: "Jurisprudence/",
           collapsible: true,
           children: "structure",
         },
+        {
+          text: "Civil Law",
+          icon: "book-open",
+          prefix: "Civil Law/",
+          link: "Civil Law/",
+          collapsible: true,
+          children: [
+            {
+              text: "General Principles",
+              prefix: "General Principles of Civil Law/",
+              link: "General Principles of Civil Law/",
+              collapsible: true,
+              children: "structure",
+            },
+            {
+              text: "Specific Provisions",
+              prefix: "Civil Law Discourse/",
+              link: "Civil Law Discourse/",
+              collapsible: true,
+              children: "structure",
+            }
+          ],
+        },
+        {
+          text: "Commercial Law",
+          icon: "building-columns",
+          prefix: "commercial-law/",
+          link: "commercial-law/",
+          collapsible: true,
+          children: "structure",
+        }
       ],
     },
-    "intro",
     {
       text: "Learning Guide",
       icon: "person-chalkboard",
       link: "https://csxf.online",
+    },
+    {
+      text: "About",
+      icon: "user",
+      link: "/intro.html",
     },
   ],
 });
