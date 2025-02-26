4e6452f88ae1ed8ba1be7088e660a7d27ad62cad
SmallQ <gongyunbo0119@outlook.com>
2025-02-26 16:04:22 +0800

    update infos

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -3,25 +3,43 @@ import { navbar } from "vuepress-theme-hope";
 export default navbar([
   "/",
   {
+    text: "个人介绍",
+    icon: "user",
+    link: "/intro"
+  },
+  {
     text: "法律学习",
-    icon: "scale-balanced",
-    prefix: "/zh/legal study/",
+    icon: "book",
+    prefix: "/legal study/",
     children: [
       {
         text: "法理学",
         icon: "gavel",
+        prefix: "Jurisprudence/",
         children: [
-          { text: "基础法理学", link: "Jurisprudence/Elementary Jurisprudence" },
-          { text: "西政法理学", link: "Jurisprudence/Elementary Jurisprudence from Northwest University of Political Science and Law" }
+          "Elementary Jurisprudence",
+          "Elementary Jurisprudence from Northwest University of Political Science and Law"
         ]
       },
       {
         text: "民商法",
         icon: "book-open",
         children: [
-          { text: "民法总论", link: "Civil Law/General Principles of Civil Law" },
-          { text: "民法分论", link: "Civil Law/Civil Law Discourse" },
-          { text: "公司法", link: "Business Law/Company Law/Company Law" }
+          {
+            text: "民法",
+            prefix: "Civil Law/",
+            children: [
+              "General Principles of Civil Law",
+              "Civil Law Discourse"
+            ]
+          },
+          {
+            text: "商法",
+            prefix: "Business Law/Company Law/",
+            children: [
+              "Company Law"
+            ]
+          }
         ]
       }
     ]
@@ -29,20 +47,10 @@ export default navbar([
   {
     text: "社会实践",
     icon: "users",
-    prefix: "/zh/social practice/",
+    prefix: "/social practice/",
     children: [
-      { text: "项目信息", link: "basic info" },
-      { text: "项目报告", link: "writing" }
+      "basic info",
+      "writing"
     ]
-  },
-  {
-    text: "崇实学辅",
-    icon: "person-chalkboard",
-    link: "https://csxf.online"
-  },
-  {
-    text: "关于我",
-    icon: "user",
-    link: "/zh/intro"
   }
 ]);
