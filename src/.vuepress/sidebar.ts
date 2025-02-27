741fe3962c339441c1515f6a6f96e9ffb2500504
SmallQ <gongyunbo0119@outlook.com>
2025-02-28 01:08:08 +0800

    Update sidebar.ts

--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -1,54 +1,66 @@
 import { sidebar } from "vuepress-theme-hope";
 
-export default sidebar({
-  "/legal-study/": [
-    {
-      text: "民法学",
-      icon: "book",
-      prefix: "civil-law/",
-      collapsible: true,
-      children: [
-        {
-          text: "民法总论",
-          icon: "book",
-          link: "General Principles of Civil Law",
-        },
-        {
-          text: "民法分论",
-          icon: "book",
-          link: "Civil Law Discourse",
-        }
-      ],
-    },
-    {
-      text: "法理学",
-      icon: "gavel",
-      prefix: "jurisprudence/",
-      collapsible: true,
-      children: [
-        {
-          text: "初级法理学",
-          icon: "book",
-          link: "Elementary Jurisprudence",
-        },
-        {
-          text: "西政法理学",
-          icon: "book",
-          link: "Elementary Jurisprudence from Northwest University of Political Science and Law",
-        }
-      ],
-    }
-  ],
-  "/life-sharing/": [
-    {
-      text: "生活分享",
-      icon: "mug-hot",
-      children: ["go-on-journey"],
-    },
-    {
-      text: "友链",
-      icon: "link",
-      link: "friend",
-    },
-  ],
-});
+export default sidebar([
+  {
+    text: "法律笔记",
+    icon: "scale-balanced",
+    prefix: "/legal-study/",
+    collapsible: true,
+    children: [
+      {
+        text: "民法学",
+        icon: "book",
+        prefix: "civil-law/",
+        collapsible: true,
+        children: [
+          {
+            text: "民法总论",
+            icon: "book",
+            link: "General Principles of Civil Law",
+          },
+          {
+            text: "民法分论",
+            icon: "book",
+            link: "Civil Law Discourse",
+          },
+        ],
+      },
+      {
+        text: "法理学",
+        icon: "gavel",
+        prefix: "jurisprudence/",
+        collapsible: true,
+        children: [
+          {
+            text: "初级法理学",
+            icon: "book",
+            link: "Elementary Jurisprudence",
+          },
+          {
+            text: "西政法理学",
+            icon: "book",
+            link: "Elementary Jurisprudence from Northwest University of Political Science and Law",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    text: "生活分享",
+    icon: "mug-hot",
+    prefix: "/life-sharing/",
+    collapsible: true,
+    children: [
+      {
+        text: "去旅行",
+        icon: "plane",
+        link: "go-on-journey",
+      },
+    ],
+  },
+  {
+    text: "友链",
+    icon: "link",
+    link: "/friend",
+  },
+]);
