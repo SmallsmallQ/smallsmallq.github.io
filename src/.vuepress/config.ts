102a1408c091b16cb45586aadd67a86b13ebede7
SmallQ <gongyunbo0119@outlook.com>
2025-03-05 00:28:29 +0800

    1.0.12

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,6 +1,25 @@
 import { defineUserConfig } from "vuepress";
-
+import { mdEnhancePlugin } from "vuepress-plugin-md-enhance"; // 使用插件
 import theme from "./theme.js";
+import ab_mdit from "any-block-converter-markdown-it"
+import jsdom from "jsdom"
+const { JSDOM } = jsdom
+const dom = new JSDOM(`<!DOCTYPE html><html><body></body></html>`, {
+  url: 'http://localhost/', // @warn 若缺少该行，则在mdit+build环境下，编译报错
+});
+// @ts-ignore 不能将类型“DOMWindow”分配给类型“Window & typeof globalThis”
+global.window = dom.window
+global.history = dom.window.history // @warn 若缺少该行，则在mdit+build环境下，编译报错：ReferenceError: history is not defined
+global.document = dom.window.document
+global.NodeList = dom.window.NodeList
+global.HTMLElement = dom.window.HTMLElement
+global.HTMLDivElement = dom.window.HTMLDivElement
+global.HTMLPreElement = dom.window.HTMLPreElement
+global.HTMLQuoteElement = dom.window.HTMLQuoteElement
+global.HTMLTableElement = dom.window.HTMLTableElement
+global.HTMLUListElement = dom.window.HTMLUListElement
+global.HTMLScriptElement = dom.window.HTMLScriptElement
+dom.window.scrollTo = ()=>{} // @warn 若缺少该行，编译警告：Error: Not implemented: window.scrollTo*/
 
 export default defineUserConfig({
   base: "/",
@@ -13,4 +32,9 @@ export default defineUserConfig({
 
   // 和 PWA 一起启用
   // shouldPrefetch: false,
+
+  // VuePress 2.x 的 Markdown 配置方式
+  extendsMarkdown: (md) => {
+    md.use(ab_mdit);
+  }
 });
--- src/README.md
+++ src/README.md
@@ -32,7 +32,6 @@ highlights:
     image: /assets/image/legal.svg
     features:
       - title: 民法学
-        icon: scale-balanced
         link: legal%20study/civil-law/
 
       - title: 法理学
